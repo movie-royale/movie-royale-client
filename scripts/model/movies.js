ad33136abde8e8a9a192ef11abf8593c16ce0d7b
brandon-fenty <brandonfenty@gmail.com>
2018-06-25 17:33:28 -0700

    adds constructors for and load functionality to models

--- scripts/model/movies.js
+++ scripts/model/movies.js
@@ -1,44 +1,67 @@
 'use strict';
 
+var app = app || {};
+
+(function(module) {
+
+// Constructor and rendering
+function Movies(moviesObj) {
+    Object.keys(moviesObj).forEach(key => this[key] = moviesObj[key]); 
+};
+
+Movies.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#insert-template-name').text());
+    return template(this);
+};
+
+// Load instances
+Movies.all = [];
+
+// AJAX fetch and load
+Movies.loadAll = rows => {
+    Movies.all = rows.map((movie) => new Movies(movie));
+};
+
 Movies.fetchAll = callback => {
-    $.get('http://localhost:5000/api/v1/movies')
+    $.get(`${app.ENV.apiURL}/api/v1/movies`)
         .then(results => {
             Movies.loadAll(results);
             callback();
         })
 };
 
-
 Movies.fetchOne = (id, callback) => {
     $.ajax({
-        url: `http://localhost:5000/api/v1/movies/${id}`,
+        url: `${app.ENV.apiURL}/api/v1/movies/${id}`,
         method: 'GET',
         data: {
             Movies_id: 1, // need to pass a variable in here to select specific movies id
             }
     })
         .then(results => {
-            Movies.loadOne(results);
+            Movies.loadAll(results);
             callback();
         })
 };
 
+// Delete Movies
 Movies.deleteOne = (id, callback) => {
     $.ajax({
-        url: `http://localhost:5000/api/v1/movies/${id}`,
+        url: `${app.ENV.apiURL}/api/v1/movies/${id}`,
         method: 'DELETE',
         data: {
             Movies_id: 1, // need to pass a variable in here to select specific movies id
             }
     })
         .then(results => {
-            Movies.loadOne(results);
+            Movies.loadAll(results);
             callback();
         })
 };
 
+// Post new movie
 Movies.prototype.postOne = function (callback) {
-    $.post('http://localhost:5000/api/v1/movies', {
+    $.post(`${app.ENV.apiURL}/api/v1/movies`, {
         title: this.title,
         rating: this.rating,
         description: this.description
@@ -48,3 +71,7 @@ Movies.prototype.postOne = function (callback) {
         callback();
     })
 };
+
+module.Movies = Movies;
+
+})(app);
